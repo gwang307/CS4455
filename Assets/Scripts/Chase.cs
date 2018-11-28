c7b93e997fbb796dcbef4d0dbe6fa27c89791718
Joseph Cho <josephcho32@gmail.com>
2018-11-27 22:14:48 -0500

    Enemy update
    Gave enemies areas to patrol around

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -1,20 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+//[RequireComponent(typeof(NavMeshAgent))]
 public class Chase : MonoBehaviour {
 
     public Transform player;
+    //private NavMeshAgent nma;
     static Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject[] waypoints;
+    public int currWaypoint = -1;
+
+    private void SetNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+        {
+            throw (new System.IndexOutOfRangeException("Array size 0"));
+        }
+        currWaypoint++;
+        if (currWaypoint >= waypoints.Length)
+        {
+            currWaypoint = 0;
+        }
+        //nma.SetDestination(waypoints[currWaypoint].transform.position);
+    }
+
+    // Use this for initialization
+    void Start () {
         anim = GetComponent<Animator>();
+        //nma = GetComponent<NavMeshAgent>();
+        SetNextWaypoint();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        // player is near
         if (Vector3.Distance(player.position, this.transform.position) < 10)
         {
             Vector3 direction = player.position - this.transform.position;
@@ -33,9 +55,20 @@ public class Chase : MonoBehaviour {
                 anim.SetBool("isWalking", false);
             }
         }
+        // player far; chase balls
         else
         {
-            anim.SetBool("isWalking", false);
+            Vector3 wayDir = waypoints[currWaypoint].transform.position - this.transform.position;
+            wayDir.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(wayDir), 0.1f);
+            anim.SetBool("isWalking", true);
+            this.transform.Translate(0, 0, 0.1f);
+            if (Vector3.Distance(this.transform.position, waypoints[currWaypoint].transform.position) <= 5)
+            //if (nma.remainingDistance <= 0.1 && nma.pathPending != true)
+            {
+                SetNextWaypoint();
+            }
+            //anim.SetFloat("vely", nma.velocity.magnitude / nma.speed);
         }
 
     }
