9f331b69834759cc7037ce9bbae60e10c63109fa
Jay Ji <bjheng919@126.com>
2018-12-12 05:50:29 -0500

    Fixed water fall through & start menu

---
+++ Assets/Scripts/TriggerBased/SuddenKillWater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuddenKillWater : MonoBehaviour {
+
+	Transform player;
+
+	void Start() {
+		player = GameObject.Find("MC2").GetComponent<Transform>();
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.attachedRigidbody != null) {
+			Collector collector = c.attachedRigidbody.gameObject.GetComponent<Collector>();
+			if (collector != null) {
+				player.position = new Vector3(-136.98f, 12, 169.3f);
+				collector.score = Mathf.Max(collector.score - 500, 0);
+				collector.updateScoreUI();
+			}
+		}
+	}
+
+}
